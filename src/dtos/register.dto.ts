66c434b19f6690c0958168950bec8e8538b88ace
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-04-22 19:44:23 +0200

    add register dto

---
+++ src/dtos/register.dto.ts
@@ -0,0 +1,9 @@
+export type RegisterDto = {
+    firstName: string,
+    lastName: string,
+    role: string,
+    email: string,
+    password: string,
+    confirmPassword: string,
+    tutorDescription: string
+}
