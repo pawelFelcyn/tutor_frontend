016cd36d6fe188356983a350f185e936fed41ecb
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-04-22 20:25:48 +0200

    registration model bindings

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterDto } from 'src/dtos/register.dto';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerDto: RegisterDto = {};
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public submit(): void{
+    console.log(this.registerDto);
+  }
 }
--- src/dtos/register.dto.ts
+++ src/dtos/register.dto.ts
@@ -1,9 +1,9 @@
 export type RegisterDto = {
-    firstName: string,
-    lastName: string,
-    role: string,
-    email: string,
-    password: string,
-    confirmPassword: string,
-    tutorDescription: string
+    firstName?: string,
+    lastName?: string,
+    role?: string,
+    email?: string,
+    password?: string,
+    confirmPassword?: string,
+    tutorDescription?: string
 }
