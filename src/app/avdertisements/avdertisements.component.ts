356cdbd1e5baeda3c686edc42861dc89b70ea7a1
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-04-26 20:04:06 +0200

    basic layout for ads

--- src/app/avdertisements/avdertisements.component.ts
+++ src/app/avdertisements/avdertisements.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdvertisementDto } from 'src/dtos/advertisement.dto';
 
 @Component({
   selector: 'app-avdertisements',
@@ -7,7 +8,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvdertisementsComponent implements OnInit {
 
-  constructor() { }
+  advertisements: AdvertisementDto[] = [];
+
+  constructor() {
+    this.seedAdvertisements();
+  }
+
+  private seedAdvertisements(): void{
+    this.advertisements.push({
+      id: "1",
+      title: 'first title',
+      creationDate: new Date(),
+      lastModificationDate: new Date(),
+      pricePerHour: 10
+    });
+    this.advertisements.push({
+      id: "2",
+      title: 'second title',
+      creationDate: new Date(),
+      lastModificationDate: new Date(),
+      pricePerHour: 10
+    });
+    this.advertisements.push({
+      id: "3",
+      title: 'third title',
+      creationDate: new Date(),
+      lastModificationDate: new Date(),
+      pricePerHour: 10
+    });
+    this.advertisements.push({
+      id: "4",
+      title: 'fourth title',
+      creationDate: new Date(),
+      lastModificationDate: new Date(),
+      pricePerHour: 10
+    });
+    this.advertisements.push({
+      id: "5",
+      title: 'fifth title',
+      creationDate: new Date(),
+      lastModificationDate: new Date(),
+      pricePerHour: 10
+    });
+    this.advertisements.push({
+      id: "1",
+      title: 'first title',
+      creationDate: new Date(),
+      lastModificationDate: new Date(),
+      pricePerHour: 10
+    });
+    this.advertisements.push({
+      id: "2",
+      title: 'second title',
+      creationDate: new Date(),
+      lastModificationDate: new Date(),
+      pricePerHour: 10
+    });
+    this.advertisements.push({
+      id: "3",
+      title: 'third title',
+      creationDate: new Date(),
+      lastModificationDate: new Date(),
+      pricePerHour: 10
+    });
+    this.advertisements.push({
+      id: "4",
+      title: 'fourth title',
+      creationDate: new Date(),
+      lastModificationDate: new Date(),
+      pricePerHour: 10
+    });
+    this.advertisements.push({
+      id: "5",
+      title: 'fifth title',
+      creationDate: new Date(),
+      lastModificationDate: new Date(),
+      pricePerHour: 10
+    });
+  }
 
   ngOnInit(): void {
   }
